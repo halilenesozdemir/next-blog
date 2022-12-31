bffd44f7096ab2ba659ec7afb4e5dcd08c2126e3
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-12-31 17:00:35 +0300

    Nextjs Blog Project Completed.

---
+++ app/layout.js
@@ -0,0 +1,19 @@
+import styles from './styles.module.css';
+import '../styles/globals.css';
+import { Inter } from '@next/font/google';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const interFont = Inter();
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='tr' className={interFont.className}>
+      <body className={styles.container}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
